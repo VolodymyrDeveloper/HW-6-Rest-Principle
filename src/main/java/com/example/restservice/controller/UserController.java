e7538258e7174b48f529ee49ec6c4ff81bcc65b5
VolodymyrDeveloper <xalimonenko@gmail.com>
2022-02-07 16:47:04 +0200

    fixed the method of adding a user

--- src/main/java/com/example/restservice/controller/UserController.java
+++ src/main/java/com/example/restservice/controller/UserController.java
@@ -2,13 +2,7 @@ package com.example.restservice.controller;
 
 import com.example.restservice.model.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.ArrayList;
@@ -38,17 +32,20 @@ public class UserController {
 
     @PostMapping("/users")
     public ResponseEntity<User> addUser(@RequestBody User user) {
-        if (users.stream().anyMatch(user1 -> user1.getId() == user.getId())) {
+        if (users.stream().anyMatch(user1 -> user1.getId() == user.getId() ||
+                user1.getSurname().equalsIgnoreCase(user.getSurname()))) {
             return ResponseEntity.badRequest().build();
+        } else {
+            users.add(user);
+            return ResponseEntity.created(URI.create("/users")).build();
         }
-        users.add(user);
-        return ResponseEntity.created(URI.create("/users")).build();
+
     }
 
     @PutMapping("/users/{id}")
     public ResponseEntity<User> updateUser(@RequestBody User userBody, @PathVariable("id") int id) {
         for (User user : users) {
-            if(user.getId() == id) {
+            if (user.getId() == id) {
                 user.setAge(userBody.getAge());
                 user.setName(userBody.getName());
                 user.setSurname(userBody.getSurname());
